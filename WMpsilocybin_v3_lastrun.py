67f37d7f87e36f7f8ef66a75a8e2d32acfa74371
Caleb Jerinic-Brodeur <111454218+cjerinic@users.noreply.github.com>
2026-01-14 17:01:29 -0600

    checks on laptop + spelling fixes

--- WMpsilocybin_v3_lastrun.py
+++ WMpsilocybin_v3_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2025.2.4),
-    on Tue Jan 13 13:49:41 2026
+    on Wed Jan 14 16:54:19 2026
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
